0041bfaa822040dd172ae9d6490f6a97ff87ab5b
boris94kg <boris94serbia@gmail.com>
2019-08-13 00:36:23 +0200

    footer fixed

--- src/view/partials/Footer.js
+++ src/view/partials/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer = (props) => {
     return (
         <footer className="container-fluid">
-            <p>&copy; {new Date().getFullYear()} Copyright BIT</p>
+            <p className="text-center">&copy; {new Date().getFullYear()} Copyright BIT</p>
         </footer>
 
     );
